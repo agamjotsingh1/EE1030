609298e2f3a925999b6ae0d197565bff7dd17202
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2024-09-16 19:07:09 +0530

    matgeo q6 (1)

--- matgeo_questions/c_code/funcs.c
+++ matgeo_questions/c_code/funcs.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include "funcs.h"
 
-float **lineFromPts(int n, float x1, float y1, float z1, float x2, float y2, float z2) {
+float **lineFromPts3D(int n, float x1, float y1, float z1, float x2, float y2, float z2) {
     float **pts = (float **) malloc(sizeof(float *) * n); 
 
     for(int i = 0; i < n; i++){
@@ -11,6 +11,19 @@ float **lineFromPts(int n, float x1, float y1, float z1, float x2, float y2, flo
         pts[i][0] = x1 + ((x2 - x1)*i)/n; 
         pts[i][1] = y1 + ((y2 - y1)*i)/n; 
         pts[i][2] = z1 + ((z2 - z1)*i)/n; 
+    }
+
+    return pts;
+}
+
+
+float **lineFromPts2D(int n, float x1, float y1, float x2, float y2) {
+    float **pts = (float **) malloc(sizeof(float *) * n); 
+
+    for(int i = 0; i < n; i++){
+        pts[i] = (float *) malloc(sizeof(float) * 2);
+        pts[i][0] = x1 + ((x2 - x1)*i)/n; 
+        pts[i][1] = y1 + ((y2 - y1)*i)/n; 
     }
 
     return pts;
@@ -40,6 +53,31 @@ float **normPlot(int n, int p, float x, float y, float r) {
     }
 
     return relpts;
+}
+
+float **triGet(int n, float x1, float y1, float x2, float y2, float x3, float y3) {
+    float **pts = (float **) malloc(sizeof(float *) * 3*n); 
+    float **linePts1 = lineFromPts2D(n, x1, y1, x2, y2); 
+    float **linePts2 = lineFromPts2D(n, x3, y3, x2, y2); 
+    float **linePts3 = lineFromPts2D(n, x1, y1, x3, y3); 
+
+    for(int i = 0; i < 3*n; i++){
+        pts[i] = (float *) malloc(sizeof(float *) * 2);
+        if(i < n){
+            pts[i][0] = linePts1[i][0];
+            pts[i][1] = linePts1[i][1];
+        }
+        else if(i >= n & i < 2*n){
+            pts[i][0] = linePts2[i - n][0];
+            pts[i][1] = linePts2[i - n][1];
+        }
+        else {
+            pts[i][0] = linePts3[i - 2*n][0];
+            pts[i][1] = linePts3[i - 2*n][1];
+        }
+    }
+
+    return pts;
 }
 
 void free_multi_memory(float **arr, int n){
--- matgeo_questions/c_code/funcs.h
+++ matgeo_questions/c_code/funcs.h
@@ -2,7 +2,9 @@
 #define FUNCS_H_
 
 float **lineGet(int, float, float, float, float, float);
-float **lineFromPts(int, float, float, float, float, float, float);
+float **triGet(int, float, float, float, float, float, float);
+float **lineFromPts3D(int, float, float, float, float, float, float);
+float **lineFromPts2D(int, float, float, float, float);
 float **pointsGet();
 float **normPlot(int, int, float, float, float);
 void free_multi_memory(float**, int);
