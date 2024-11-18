7651e9932f7c90e6de1eb73901a39dc54ef67c20
agamjotsingh1 <agamjot.s.kohli@gmail.com>
2024-11-18 22:25:54 +0530

    eigen final

--- eigen/code/matrix.h
+++ eigen/code/matrix.h
@@ -6,12 +6,13 @@
 
 typedef double complex compl;
 
+// Prints the Matrix
 void mprint(compl** mat, int m, int n){
     printf("[");
     for(int i = 0; i < m; i++){
         printf("[");
         for(int j = 0; j < n; j++){
-            printf("%lf + %lfi", creal(mat[i][j]), cimag(mat[i][j]));
+            printf("%.6lf + %.6lfi", creal(mat[i][j]), cimag(mat[i][j]));
             if(j != n - 1) printf(", ");
         }
         printf("]");
@@ -20,6 +21,7 @@ void mprint(compl** mat, int m, int n){
     printf("]\n\n");
 }
 
+// Returns a matrix with size mxn with all zero entries
 compl** mzeroes(int m, int n){
     compl** mat = (compl**) malloc(sizeof(compl)*m);
     for(int i = 0; i < m; i++){
@@ -32,31 +34,38 @@ compl** mzeroes(int m, int n){
     return mat;
 }
 
+// Checks equality of matrices
+int mequals(compl** mat1, compl** mat2, int m, int n){
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            if(mat1[i][j] != mat2[i][j]) return 0;
+        }
+    }
+    return 1;
+}
+
+// Returns the identity matrix with size mxm
 compl** meye(int m){
     compl** mat = mzeroes(m, m);
-    for(int i = 0; i < m; i++){
-        mat[i][i] = 1 + 0*I;
-    }
+    for(int i = 0; i < m; i++) mat[i][i] = 1 + 0*I;
     return mat;
 }
 
+// Returns the scaled matrix with scaling factor as k
 compl** mscale(compl** mat, int m, int n, compl k){
-    compl** newmat = (compl**) malloc(sizeof(compl)*m);
+    compl** newmat = mzeroes(m, n);
     for(int i = 0; i < m; i++){
-        newmat[i] = (compl*) malloc(sizeof(compl)*n);
-
         for(int j = 0; j < n; j++){
-            newmat[i][j] = k*mat[i][j];
+            newmat[i][j] = k*(mat[i][j]);
         }
     }
     return newmat;
 }
 
+// Adds two matrices and returns it
 compl** madd(compl** mat1, compl** mat2, int m, int n){
-    compl** newmat = (compl**) malloc(sizeof(compl)*m);
+    compl** newmat = mzeroes(m, n);
     for(int i = 0; i < m; i++){
-        newmat[i] = (compl*) malloc(sizeof(compl)*n);
-
         for(int j = 0; j < n; j++){
             newmat[i][j] = mat1[i][j] + mat2[i][j];
         }
@@ -64,11 +73,10 @@ compl** madd(compl** mat1, compl** mat2, int m, int n){
     return newmat;
 }
 
+// Takes complex transpose of the matrix and returns it
 compl** mT(compl** mat, int m, int n){
-    compl** newmat = (compl**) malloc(sizeof(compl)*n);
+    compl** newmat = mzeroes(n, m);
     for(int i = 0; i < n; i++){
-        newmat[i] = (compl*) malloc(sizeof(compl)*m);
-
         for(int j = 0; j < m; j++){
             newmat[i][j] = conj(mat[j][i]);
         }
@@ -76,6 +84,7 @@ compl** mT(compl** mat, int m, int n){
     return newmat;
 }
 
+// Returns the multiplication of two matrices with size mxn and nxr
 compl** mmul(compl** mat1, compl** mat2, int m, int n, int r){
     compl** newmat = mzeroes(m , r);
 
@@ -90,22 +99,20 @@ compl** mmul(compl** mat1, compl** mat2, int m, int n, int r){
     return newmat;
 }
 
+// Returns the 'k'th column of the matrix of size mxn
 compl** mgetcol(compl** mat, int m, int n, int k){
-    compl** newmat = (compl**) malloc(sizeof(compl *)*m);
-
+    compl** newmat = mzeroes(m , 1);
     for(int i = 0; i < m; i++){
-        newmat[i] = (compl*) malloc(sizeof(compl));
         newmat[i][0] = mat[i][k]; 
     }
 
     return newmat;
 }
 
+// Returns the copy of the matrix
 compl** mdup(compl** mat, int m, int n){
-    compl** newmat = (compl**) malloc(sizeof(compl)*m);
+    compl** newmat = mzeroes(m , n);
     for(int i = 0; i < m; i++){
-        newmat[i] = (compl*) malloc(sizeof(compl)*n);
-
         for(int j = 0; j < n; j++){
             newmat[i][j] = mat[i][j];
         }
@@ -113,52 +120,25 @@ compl** mdup(compl** mat, int m, int n){
     return newmat;
 }
 
-compl** vconj(compl** v, int m){
-    compl** vconj = (compl**) malloc(sizeof(compl)*m);
-
-    for(int i = 0; i < m; i++){
-        vconj[i] = (compl*) malloc(sizeof(compl)*1);
-        vconj[i][0] = conj(v[i][0]);
-    }
-
-    return vconj;
-}
-
+// Gives the norm of a m dimensional vector
 double vnorm(compl** vec, int m){
     compl** norm = mmul(mT(vec, m, 1), vec, 1, m, 1);
-
     return sqrt(creal(norm[0][0]));
 } 
 
+// Returns the m dimensional impulse vector with 1 at 'i'th position
 compl** e(int m, int i){
     compl** mat = mzeroes(m, 1);
     mat[i-1][0] = 1;
     return mat;
 }
 
-compl*** QR(compl** A, int m, int n){
-    compl** Q = mzeroes(m, n);
-    compl** R = mzeroes(n, n);
-    for(int i = 0; i < n; i++){
-        compl** a = mgetcol(A, m, n, i);
-        compl** q = mdup(a, m, 1);
-
-        for(int j = 0; j < i; j++){
-            compl x = mmul(mT(a, m, 1), mgetcol(Q, m, n, j), 1, m, 1)[0][0];
-            q = madd(q, mscale(mgetcol(Q, m, n, j), m, 1, -x), m, 1);
-            R[j][i] = x;
-        }
-
-        R[i][i] = vnorm(q, m) + 0*I;
-        q = mscale(q, m, 1, (compl) 1/vnorm(q, m));
-
-        for(int j = 0; j < m; j++){
-            Q[j][i] = q[j][0];
+// Checks if matrix is zero
+int is_zero(compl** mat, int m, int n, double tolerance){
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            if(cabs(mat[i][j]) > tolerance) return 0;
         }
     }
-
-    compl*** ret = (compl***) malloc(sizeof(compl**)*2);
-    ret[0] = Q;
-    ret[1] = R;
-    return ret;
+    return 1;
 }
